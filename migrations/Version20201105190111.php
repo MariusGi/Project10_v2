c5cf4e0c234c4a9f0386b4e07c4adc64d9b141a1
MariusGi <mariusgirnis@gmail.com>
2020-11-08 20:47:44 +0200

    change migration field type

--- migrations/Version20201105190111.php
+++ migrations/Version20201105190111.php
@@ -21,7 +21,7 @@ final class Version20201105190111 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, country_code VARCHAR(3) NOT NULL, full_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE public_holiday (id INT AUTO_INCREMENT NOT NULL, country_id INT DEFAULT NULL, year SMALLINT NOT NULL, month_day JSON NOT NULL, total_amount SMALLINT NOT NULL, INDEX IDX_8744ED86F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE public_holiday (id INT AUTO_INCREMENT NOT NULL, country_id INT DEFAULT NULL, year SMALLINT NOT NULL, month_day TEXT NOT NULL, total_amount SMALLINT NOT NULL, INDEX IDX_8744ED86F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE public_holiday ADD CONSTRAINT FK_8744ED86F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id) ON DELETE CASCADE');
     }
 
